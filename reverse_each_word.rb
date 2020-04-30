f49ce7e4606b10fe2a14e350f911158744858aca
Hyojin Yoo <yoohyojin@gmail.com>
2020-04-30 21:57:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-
-def reverse_each_word(string)
-  array = string.split.each do |word|
-    word.reverse!
+ =begin
+  def reverse_each_word(string)
+    array = string.split.each do |word|
+      word.reverse!
+    end
+    array.join(" ")
   end
-  array.join(" ")
-end
-
+=end
 
 def reverse_each_word(string)
   array = string.split.collect do |word|
